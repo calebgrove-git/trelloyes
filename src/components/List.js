16b178dddd0357fe8bc4f41a7aca95bb3ebb8960
Caleb Grove <calebgrove41@gmail.com>
2020-08-03 14:59:50 -0400

    inital commit

---
+++ src/components/List.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './Card';
+import STORE from '../Store';
+import './List.css';
+
+function List(props) {
+  console.log(props.cards);
+  const cards = props.cards.map((card, i) => (
+    <Card
+      key={i}
+      title={STORE.allCards[card].title}
+      content={STORE.allCards[card].content}
+    ></Card>
+  ));
+  return (
+    <section className='List'>
+      <header className='List-header'>
+        <h2>{props.header}</h2>
+      </header>
+      <div className='List-cards'>
+        {cards}
+        <button type='button' class='List-add-button'>
+          + Add Random Card
+        </button>
+      </div>
+    </section>
+  );
+}
+export default List;
